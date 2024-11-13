84f0e9204f85092df63af899b7d07ffc90b3b8b6
Randall Jaffe <admin@rajwebconsulting.com>
2024-11-12 23:25:38 -0600

    CSS style fixes

--- neuhub-tab/php/default.php
+++ neuhub-tab/php/default.php
@@ -118,8 +118,8 @@ if (file_exists($filename)) {
     // ! Assumes you have uploaded both the main CSS and the CSS for the sections.
     $filename = '/custom/css/codestitch.css';
     if (file_exists($filename)) { ?>
-        <link href="/custom/css/codestitch.css?1685973381" rel="stylesheet"/>
-        <link href="/custom/css/codestitch-sections.css?1685973381" rel="stylesheet"/>
+        <!--<link href="/custom/css/codestitch.css?1685973381" rel="stylesheet"/>
+        <link href="/custom/css/codestitch-sections.css?1685973381" rel="stylesheet"/>-->
     <?php
     } else {
         //// echo "The file $filename does not exist";
@@ -134,8 +134,8 @@ if (file_exists($filename)) {
         <link href="/view/theme/neuhub-tab/custom/css/codestitch.css?1685973381" rel="stylesheet"/>
         <link href="/view/theme/neuhub-tab/custom/css/codestitch-sections.css?1685973381" rel="stylesheet"/>
   -->
-        <link href="/view/theme/neuhub-tab/css/codestitch.css?1685973381" rel="stylesheet"/>
-        <link href="/view/theme/neuhub-tab/css/codestitch-sections.css?1685973381" rel="stylesheet"/>
+        <!--<link href="/view/theme/neuhub-tab/css/codestitch.css?1685973381" rel="stylesheet"/>
+        <link href="/view/theme/neuhub-tab/css/codestitch-sections.css?1685973381" rel="stylesheet"/>-->
 
     <style>
       /* @import url('/view/theme/neuhub-tab/assets/fonts/inter/inter.css'); */
